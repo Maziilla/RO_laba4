91b194acae8f1bfc5bf3b099251a578af2328259
михаил юрков <mishayur1997@mail.ru>
2018-04-04 01:32:49 +0500

    Добавьте файлы проекта.

---
+++ Laba1/Form1.Designer.cs
@@ -0,0 +1,252 @@
+﻿namespace Laba1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.dataSet1 = new System.Data.DataSet();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.tb_FEr = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.tb_Ri = new System.Windows.Forms.TextBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.tb_SEr = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.cb_rast = new System.Windows.Forms.ComboBox();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.groupBox5 = new System.Windows.Forms.GroupBox();
+            this.l_name = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)(this.dataSet1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox4.SuspendLayout();
+            this.groupBox5.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataSet1
+            // 
+            this.dataSet1.DataSetName = "NewDataSet";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.tb_FEr);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(135, 52);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Ошибки первого рода";
+            // 
+            // tb_FEr
+            // 
+            this.tb_FEr.Location = new System.Drawing.Point(15, 19);
+            this.tb_FEr.Name = "tb_FEr";
+            this.tb_FEr.Size = new System.Drawing.Size(100, 20);
+            this.tb_FEr.TabIndex = 0;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.tb_Ri);
+            this.groupBox2.Location = new System.Drawing.Point(12, 128);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(135, 52);
+            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Верно";
+            // 
+            // tb_Ri
+            // 
+            this.tb_Ri.Location = new System.Drawing.Point(15, 21);
+            this.tb_Ri.Name = "tb_Ri";
+            this.tb_Ri.Size = new System.Drawing.Size(100, 20);
+            this.tb_Ri.TabIndex = 2;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.tb_SEr);
+            this.groupBox3.Location = new System.Drawing.Point(12, 70);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(135, 52);
+            this.groupBox3.TabIndex = 2;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Ошибки второго рода";
+            this.groupBox3.Enter += new System.EventHandler(this.groupBox3_Enter);
+            // 
+            // tb_SEr
+            // 
+            this.tb_SEr.Location = new System.Drawing.Point(15, 19);
+            this.tb_SEr.Name = "tb_SEr";
+            this.tb_SEr.Size = new System.Drawing.Size(100, 20);
+            this.tb_SEr.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(204, 86);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Распознать";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // cb_rast
+            // 
+            this.cb_rast.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cb_rast.FormattingEnabled = true;
+            this.cb_rast.Items.AddRange(new object[] {
+            "Метод удержания",
+            "Перекрёстная проверка"});
+            this.cb_rast.Location = new System.Drawing.Point(17, 19);
+            this.cb_rast.Name = "cb_rast";
+            this.cb_rast.Size = new System.Drawing.Size(100, 21);
+            this.cb_rast.TabIndex = 4;
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.cb_rast);
+            this.groupBox4.Location = new System.Drawing.Point(175, 12);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(135, 52);
+            this.groupBox4.TabIndex = 4;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "Количество полосок";
+            // 
+            // groupBox5
+            // 
+            this.groupBox5.Controls.Add(this.l_name);
+            this.groupBox5.Location = new System.Drawing.Point(175, 128);
+            this.groupBox5.Name = "groupBox5";
+            this.groupBox5.Size = new System.Drawing.Size(135, 52);
+            this.groupBox5.TabIndex = 5;
+            this.groupBox5.TabStop = false;
+            this.groupBox5.Text = "Ваш образец это";
+            // 
+            // l_name
+            // 
+            this.l_name.AutoSize = true;
+            this.l_name.Location = new System.Drawing.Point(26, 21);
+            this.l_name.Name = "l_name";
+            this.l_name.Size = new System.Drawing.Size(0, 13);
+            this.l_name.TabIndex = 0;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(12, 199);
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(135, 20);
+            this.numericUpDown1.TabIndex = 3;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // chart1
+            // 
+            chartArea1.AxisX.IsStartedFromZero = false;
+            chartArea1.AxisX2.IsStartedFromZero = false;
+            chartArea1.AxisY.IsStartedFromZero = false;
+            chartArea1.AxisY2.IsStartedFromZero = false;
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(362, 12);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(258, 273);
+            this.chart1.TabIndex = 6;
+            this.chart1.Text = "chart1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(746, 282);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.groupBox5);
+            this.Controls.Add(this.groupBox4);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Распознавание животных";
+            ((System.ComponentModel.ISupportInitialize)(this.dataSet1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.groupBox4.ResumeLayout(false);
+            this.groupBox5.ResumeLayout(false);
+            this.groupBox5.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Data.DataSet dataSet1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox cb_rast;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.GroupBox groupBox5;
+        private System.Windows.Forms.Label l_name;
+        private System.Windows.Forms.TextBox tb_FEr;
+        private System.Windows.Forms.TextBox tb_Ri;
+        private System.Windows.Forms.TextBox tb_SEr;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+    }
+}
+
---
+++ Laba1/Form1.cs
@@ -0,0 +1,277 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Laba1
+{
+    public partial class Form1 : Form
+    {
+        DataSet Learn,Test;
+
+        public void add_animal(DataTable table, double a1, double a2, double a3, double a4, string a5)
+        {
+            table.Rows.Add(a5, a1, a2, a3, a4);
+        }      
+      
+        public void add_col(DataTable table)
+        {
+            DataColumn col_bee =
+               table.Columns.Add("name", typeof(string));
+            table.Columns.Add("sepal length", typeof(double));
+            table.Columns.Add("sepal width", typeof(double));
+            table.Columns.Add("petal length", typeof(double));
+            table.Columns.Add("petal width", typeof(double));
+        }
+        public double Evkl_dist(DataRow row, double[] row1)
+        {
+            return Math.Sqrt(Math.Pow((double)row.ItemArray[1] - row1[0], 2) + Math.Pow((double)row.ItemArray[2] - row1[1], 2) + Math.Pow((double)row.ItemArray[3] -
+                row1[2], 2) + Math.Pow((double)row.ItemArray[4] - row1[3], 2));
+        }
+        public double Evkl_dist(DataRow row, DataRow row1)
+        {
+            return Math.Sqrt(Math.Pow((double)row.ItemArray[1] - (double)row1.ItemArray[1], 2) + Math.Pow((double)row.ItemArray[2] - (double)row1.ItemArray[2], 2) +
+                Math.Pow((double)row.ItemArray[3] - (double)row1.ItemArray[3], 2) + Math.Pow((double)row.ItemArray[4] - (double)row1.ItemArray[4], 2));
+        }
+        public int Choise(DataRow Row)
+        {
+            double min_dist = Double.MaxValue, dist = 0;
+            string Name = "";
+            foreach (DataTable tabl in Learn.Tables)
+            {
+                foreach (DataRow row in tabl.Rows)
+                {
+                    dist = Evkl_dist(row,Row);
+                    
+                    if (dist < min_dist)
+                    {
+                        min_dist = dist;
+                        Name = (string)row.ItemArray[0];
+                    }
+
+                }
+            }
+
+            if ((string)Row.ItemArray[0] == Name && (string)Row.ItemArray[0] == "Iris - versicolor")
+                return 1;
+            else if ((string)Row.ItemArray[0] == Name)
+                return 2;
+            else
+                if ((string)Row.ItemArray[0] == "Iris-versicolor")
+                return 3;//1 род
+            else
+                return 4;//2 род
+            //l_name.Text = Name;
+        }
+        //table.Rows.Add(nUD_pol.Value, nUD_hight.Value, nUD_long.Value);
+        public Form1()
+        {
+            InitializeComponent();
+            cb_rast.SelectedIndex = 0;
+            Test = new DataSet("Test");
+            Learn = new DataSet("Learn");
+        }
+
+        private void groupBox3_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            chart1.Series[0].Points.Clear();
+           StreamReader textReader;
+            string sLine = "";
+            string[] split;
+            double[] znach = new double[4];
+            double Fn=0, Fp=0, Right=0,tn=0,tp=0;
+            int typeEr;
+            int j;
+            double TPR;
+            double FPR;
+            Test.Tables.Clear();
+            Learn.Tables.Clear();
+            Learn.Tables.Add("1");
+            Test.Tables.Add("1");
+            add_col(Learn.Tables[0]);
+            add_col(Test.Tables[0]);
+            if (cb_rast.SelectedIndex == 0)
+            //------------------------------------------------------------------------------------------------------
+            {
+                Test.Clear();
+                Learn.Clear();
+                Test.Tables.Clear();
+                Learn.Tables.Clear();
+                textReader = new StreamReader("J:\\Универ\\3 курс\\2 триместр\\РО\\Laba1\\Laba1\\dataset.txt");
+                sLine = "";
+                Learn.Tables.Add("1");
+                Test.Tables.Add("1");
+                add_col(Learn.Tables[0]);
+                add_col(Test.Tables[0]);
+
+                textReader.ReadLine();
+
+                j = 0;
+                while ((sLine = textReader.ReadLine()) != null)
+                {
+                    j++;
+                    split = sLine.Split('\t');
+                    for (int i = 0; i < znach.Length; i++)
+                    {
+                        znach[i] = Convert.ToDouble(split[i]);
+                    }
+                    if (j % numericUpDown1.Value == 0)
+                        add_animal(Test.Tables[0], znach[0], znach[1], znach[2], znach[3], split[4]);
+                    else
+                        add_animal(Learn.Tables[0], znach[0], znach[1], znach[2], znach[3], split[4]);
+                }
+                
+
+                for (double T = 0; T < 45; T += 0.1)
+                {
+                    Fn = Fp = Right = tn = tp = 0;
+                    foreach (DataRow row in Test.Tables[0].Rows)
+                    {
+                        double min_dist = Double.MaxValue, dist = 0;
+                        string Name = "";
+                        foreach (DataTable tabl in Learn.Tables)
+                        {
+                            foreach (DataRow row1 in tabl.Rows)
+                            {
+                                dist = Evkl_dist(row1, row);
+
+                                if (dist < min_dist)
+                                {
+                                    min_dist = dist;
+                                    Name = (string)row1.ItemArray[0];
+                                }
+
+                            }
+                        }
+
+                        if(min_dist > T)
+                        {
+                            if ((string)row.ItemArray[0] == Name)
+                                tn++;
+                            else
+                                Fp++;
+                        }
+                        else
+                        {
+                            if ((string)row.ItemArray[0] == Name)
+                                tp++;
+                            else
+                                Fn++;
+                        }
+                        //typeEr = Choise(row);
+                        //switch (typeEr)
+                        //{
+                        //    case 1: tn++; break;
+                        //    case 2: tp++; break;
+                        //    case 3: Fn++; break;
+                        //    case 4: Fp++; break;
+                        //}
+                    }
+                    //TPR = tp / (tp + Fp + 1e-15);
+                    //FPR = tn / (tp + Fn + 1e-15);
+                    //chart1.Series[0].Points.AddXY(1 - TPR, FPR);
+                    TPR = tp / (tp + Fn + 1e-15);
+                    FPR = tn / (tp + Fp + 1e-15);
+                    chart1.Series[0].Points.AddXY(FPR, TPR);
+                }
+            }
+            //------------------------------------------------------------------------------------------
+            else
+            {
+                for (double T = 0; T < 45; T += 0.1)
+                {  Fn = Fp = Right = tn = tp = 0;
+                    sLine = "";
+                Fn = Fp = Right = tn = tp = 0;
+                for (int k = 0; k < numericUpDown1.Value; k++)
+                {
+                    Test.Tables[0].Rows.Clear();
+                    Learn.Tables[0].Rows.Clear();
+                    textReader = new StreamReader("J:\\Универ\\3 курс\\2 триместр\\РО\\Laba1\\Laba1\\dataset.txt");
+                    textReader.ReadLine();
+
+                    j = 0;
+                    while ((sLine = textReader.ReadLine()) != null)
+                    {
+                        j++;
+                        split = sLine.Split('\t');
+                        for (int i = 0; i < znach.Length; i++)
+                        {
+                            znach[i] = Convert.ToDouble(split[i]);
+                        }
+                        if (j % numericUpDown1.Value == k)
+                            add_animal(Test.Tables[0], znach[0], znach[1], znach[2], znach[3], split[4]);
+                        else
+                            add_animal(Learn.Tables[0], znach[0], znach[1], znach[2], znach[3], split[4]);
+                    }
+
+                    
+                        foreach (DataRow row in Test.Tables[0].Rows)
+                        {
+                            double min_dist = Double.MaxValue, dist = 0;
+                            string Name = "";
+                            foreach (DataTable tabl in Learn.Tables)
+                            {
+                                foreach (DataRow row1 in tabl.Rows)
+                                {
+                                    dist = Evkl_dist(row1, row);
+
+                                    if (dist < min_dist)
+                                    {
+                                        min_dist = dist;
+                                        Name = (string)row1.ItemArray[0];
+                                    }
+
+                                }
+                            }
+
+                            if (min_dist > T)
+                            {
+                                if ((string)row.ItemArray[0] == Name)
+                                    tn++;
+                                else
+                                    Fp++;
+                            }
+                            else
+                            {
+                                if ((string)row.ItemArray[0] == Name)
+                                    tp++;
+                                else
+                                    Fn++;
+                            }
+                        }
+                    }
+                    tp /= (int)numericUpDown1.Value;
+                    tn /= (int)numericUpDown1.Value;
+                    Fn /= (int)numericUpDown1.Value;
+                    Fp /= (int)numericUpDown1.Value;
+                    TPR = tp / (tp + Fn + 1e-15);
+                    FPR = tn / (tp + Fp + 1e-15);
+                    chart1.Series[0].Points.AddXY(FPR, TPR);
+                }
+            }
+            //------------------------------------------------------------------------------------------
+            tb_FEr.Text = Fn.ToString();
+            tb_SEr.Text = Fp.ToString();
+            tb_Ri.Text = Right.ToString();
+            //Choise();
+        }
+
+
+    }
+}
